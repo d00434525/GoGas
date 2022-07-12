8e11dfcc01cda3eba54b5c87b2817e74e50ec1ca
d00434525 <d00434525@dmail.dixie.edu>
2022-07-12 10:26:40 -0600

    home page

--- public/app.js
+++ public/app.js
@@ -1,15 +1,8 @@
-import Vue from 'vue'
-import Vuetify from 'vuetify'
-import 'vuetify/dist/vuetify.min.css'
-
-Vue.use(Vuetify)
-
-
 var app = new Vue({
     el: "#app",
     data:{
         // data members go here
-        page: "register",
+        page: "home",
 
         // Register Page
         rUsername: "",
